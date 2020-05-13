484009c6ebdb425cf9566b3d5aaf73cb7e0c6636
kaumonaung <ag.kaumon@gmail.com>
2020-05-13 13:18:13 +0200

    Initial Commit

---
+++ NewsBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsBook
+//
+//  Created by Kaumon Aung on 13.05.20.
+//  Copyright © 2020 Kaumon Aung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
